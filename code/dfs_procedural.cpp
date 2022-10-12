c7c8b1b87e768cb4fc03156022dadb947e5b3fd7
jordilazo <jordilazo94@gmail.com>
2022-10-12 23:15:33 +0200

    first steps c++

---
+++ code/dfs_procedural.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int visited[1000];
+
+int main(){
+    int n = 5;
+    vector<int> a[1000];
+    while(n--){
+        int p,q;
+        cin>>p>>q;
+        a[p].push_back(q);
+
+    }
+    for (auto &i : a)
+        for (auto j : i)
+            cout << "array index:" << &i - &a[0] << "  Entry " << j << endl;
+    //for (int i = 0; i < a.size(); i++) {
+        //std::cout << a.at(i) << ' ';
+    //}
+
+}
