5d764336bbc7b60cfdb523e625ae2cc20bbb79b8
jordilazo <jordilazo94@gmail.com>
2022-10-16 14:02:12 +0200

    Table and DFS pacman

--- code/class.h
+++ code/class.h
@@ -1,21 +1,52 @@
 #include<stdio.h>
+#include<stack>
+using namespace std;
+#define WALL 0
+#define PATH 1
+#define CENTERWALL 2
+
+class Position{
+    public:
+    int x;
+    int y;
+    Position(int x, int y){
+        this-> x = x;
+        this-> y = y;
+    }
+};
+
 class Table{
 public:
     int COLUMNS;
     int ROWS;
     int **array;
     Table(int columns, int rows){
-        array = new *int[rows];
+        COLUMNS = columns;
+        ROWS = rows;
+        array = new int*[rows];
         for(int i = 0; i<rows;i++){
             array[i] = new int[columns];
         }
+        GenerateTableDFS(Position(columns/2,rows/2));
     }
     void printTable(){
         for(int i = 0; i< ROWS; i++){
             for (int j = 0; j < COLUMNS; j++){
                 printf("%d",array[i][j]);
-                printf("\n");
             }
+            printf("\n");
+        }
+    }
+    void GenerateTableDFS(Position startPosition){
+        stack<Position> pila;
+        pila.push(startPosition);
+        array[startPosition.y][startPosition.x] = PATH;
+        while(!pila.empty()){
+            Position currentPosition = pila.top();
+            //buscar vecinos
+            //push vecino 1
+            //if not vecinos pop
+
         }
     }
 };
--- code/pacman.cpp
+++ code/pacman.cpp
@@ -1,8 +1,12 @@
-#include <GL/glut.h>
+#include<GL/glut.h>
+#include"class.h"
 
 #define COLUMNS 8
 #define ROWS 8
 #define WIDTH 300
 #define HEIGHT 300
 
-
+int main(){
+    Table table(COLUMNS,ROWS);
+    table.printTable();
+}
